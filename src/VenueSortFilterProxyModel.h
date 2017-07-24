05a89bca43f6bdd8a43f89d1fad57a9c982ab254
micu <micuintus@gmx.de>
2017-07-24 07:55:08 +0200

    Add filter options for venue properties
    // Github issue #31

--- src/VenueSortFilterProxyModel.h
+++ src/VenueSortFilterProxyModel.h
@@ -20,6 +20,7 @@ class VenueSortFilterProxyModel : public QSortFilterProxyModel
     Q_PROPERTY(QGeoCoordinate currentPosition MEMBER m_currentPosition WRITE setCurrentPosition)
     Q_PROPERTY(VenueModel::VenueModelCategory filterModelCategory MEMBER m_filterModelCategory WRITE setFilterModelCategory NOTIFY filterModelCategoryChanged)
     Q_PROPERTY(VenueSortFilterProxyModel::VenueVeganCategories filterVeganCategory READ filterVeganCategory NOTIFY filterVeganCategoryChanged)
+    Q_PROPERTY(VenueSortFilterProxyModel::VenueProperties filterVenueProperty READ filterVenueProperty NOTIFY filterVenuePropertyChanged)
     Q_PROPERTY(bool filterFavorites MEMBER m_filterFavorites WRITE setFilterFavorites NOTIFY filterFavoritesChanged)
 
 public:
@@ -34,12 +35,31 @@ public:
     Q_FLAG(VenueVeganCategories)
     Q_ENUM(VenueVeganCategory)
 
+    enum VenueProperty {
+        Wlan                    = keyToFlag(VenueModel::Wlan - VenueModel::Wlan),
+        HandicappedAccessible   = keyToFlag(VenueModel::HandicappedAccessible - VenueModel::Wlan),
+        HandicappedAccessibleWc = keyToFlag(VenueModel::HandicappedAccessibleWc - VenueModel::Wlan),
+        Catering                = keyToFlag(VenueModel::Catering - VenueModel::Wlan),
+        Organic                 = keyToFlag(VenueModel::Organic - VenueModel::Wlan),
+        GlutenFree              = keyToFlag(VenueModel::GlutenFree - VenueModel::Wlan),
+        Delivery                = keyToFlag(VenueModel::Delivery - VenueModel::Wlan),
+        Dog                     = keyToFlag(VenueModel::Dog - VenueModel::Wlan),
+        ChildChair              = keyToFlag(VenueModel::ChildChair - VenueModel::Wlan)
+    };
+    Q_DECLARE_FLAGS(VenueProperties, VenueProperty)
+    Q_FLAG(VenueProperties)
+    Q_ENUM(VenueProperty)
+
     VenueSortFilterProxyModel(QObject *parent = 0);
 
     Q_INVOKABLE VenueModel* model() const;
+    Q_INVOKABLE VenueVeganCategories filterVeganCategory() const;
+    Q_INVOKABLE VenueProperties filterVenueProperty() const;
+
     Q_INVOKABLE QVariantMap item(int row) const;
-    VenueVeganCategories filterVeganCategory() const;
     Q_INVOKABLE void setVeganCategoryFilterFlag(VenueVeganCategory flag, bool on);
+    Q_INVOKABLE void setVenuePropertyFilterFlag(VenueProperty flag, bool on);
+
 
 public slots:
     void setModel(VenueModel* model);
@@ -52,8 +72,9 @@ signals:
     void modelChanged(VenueModel* model);
     void searchStringChanged(QString);
     void filterModelCategoryChanged(VenueModel::VenueModelCategory);
-    void filterFavoritesChanged(bool);
     void filterVeganCategoryChanged(VenueVeganCategories);
+    void filterVenuePropertyChanged(VenueProperties);
+    void filterFavoritesChanged(bool);
 
 protected:
     bool filterAcceptsRow(int source_row, const QModelIndex &source_parent) const override;
@@ -65,10 +86,15 @@ private:
     bool searchStringMatches(const QModelIndex& index) const;
     bool modelCategoryMatches(const QModelIndex& index) const;
     bool veganCategoryMatches(const QModelIndex& index) const;
+    bool venuePropertyMatches(const QModelIndex& index) const;
 
     QString m_searchString;
     QGeoCoordinate m_currentPosition;
     VenueModel::VenueModelCategory m_filterModelCategory = VenueModel::Food;
+    // positive filter
     VenueVeganCategories m_filterVeganCategory = { VenueVeganCategory::Vegan | VenueVeganCategory::Vegetarian | VenueVeganCategory::Omnivore };
+    // negative filter
+    VenueProperties m_filterVenueProperty = { };
+
     bool m_filterFavorites = false;
 };
