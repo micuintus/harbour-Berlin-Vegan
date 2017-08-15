micuintus/harbour-Berlin-Vegan

commit 38538533a34b47aeb0f7ec017e564ae4883d50e0

[SailfishOS] Small refacoring,
which prepares for bringing back the hot opacity
transition on SailfishOS.

// Github issue #122

qml/pages/VenueDescription.qml
@@ -40,14 +40,14 @@ BVApp.Page {
     SilicaFlickable {
         id: flicka
         anchors.fill: parent
-        readonly property var nonDescriptionHeaderHeight: locationheader.height + iconToolBar.height
         contentHeight: descriptionText.y + descriptionText.height + BVApp.Theme.paddingLarge
+        readonly property real nonDescriptionHeaderHeight: locationHeader.height
+                                                          + underHeaderBar.height
+                                                          + iconToolBar.height
         property real scrolledUpRatio: 1 - (contentY / nonDescriptionHeaderHeight)
 
-        VerticalScrollDecorator {}
-
         BVApp.VenueDescriptionHeader {
-            id: locationheader
+            id: locationHeader
             name: restaurant.name
             pictures: restaurant.pictures
             positionSource: page.positionSource
@@ -65,47 +65,55 @@ BVApp.Page {
             }
         }
 
-        Label {
-            id: streetLabel
-            text: restaurant.street
-            font.pixelSize: BVApp.Theme.fontSizeExtraSmall
-            color: BVApp.Platform.isSailfish ? BVApp.Theme.highlightColor : BVApp.Theme.secondaryColor
-            truncationMode: TruncationMode.Fade
+        Item {
+            id: underHeaderBar
+
+            opacity: flicka.scrolledUpRatio
+            width: parent.width
+            height: streetLabel.height
 
             anchors {
-                left: parent.left
-                right: distanceLabel.left
-                top: locationheader.bottom
+                top: locationHeader.bottom
 
-                leftMargin: BVApp.Theme.horizontalPageMargin
-                rightMargin: BVApp.Theme.horizontalPageMargin
                 topMargin: BVApp.Theme.paddingMedium
                 bottomMargin: BVApp.Theme.paddingMedium
             }
 
-            opacity: flicka.scrolledUpRatio
-        }
+            Label {
+                id: streetLabel
+                text: restaurant.street
+                font.pixelSize: BVApp.Theme.fontSizeExtraSmall
+                color: BVApp.Platform.isSailfish ?
+                         BVApp.Theme.highlightColor
+                       : BVApp.Theme.secondaryColor
+                truncationMode: TruncationMode.Fade
 
-        Label {
-            id: distanceLabel
-            text: positionSource.supportedPositioningMethods !== PositionSource.NoPositioningMethods
-                  ? BVApp.DistanceAlgorithms.humanReadableDistanceString(positionSource.position.coordinate,
-                    QtPositioning.coordinate(restaurant.latCoord, restaurant.longCoord))
-                  : ""
-            font.pixelSize: BVApp.Theme.fontSizeExtraSmall
-            color: BVApp.Theme.highlightColor
+                anchors {
+                    top: parent.top
+                    left: parent.left
 
-            anchors {
-                right: parent.right
-                top: locationheader.bottom
-
-                leftMargin: BVApp.Theme.horizontalPageMargin
-                rightMargin: BVApp.Theme.horizontalPageMargin
-                topMargin: BVApp.Theme.paddingMedium
-                bottomMargin: BVApp.Theme.paddingMedium
+                    leftMargin: BVApp.Theme.horizontalPageMargin
+                }
             }
 
-            opacity: flicka.scrolledUpRatio
+            Label {
+                id: distanceLabel
+                text: positionSource.supportedPositioningMethods !== PositionSource.NoPositioningMethods
+                      ? BVApp.DistanceAlgorithms.humanReadableDistanceString(positionSource.position.coordinate,
+                        QtPositioning.coordinate(restaurant.latCoord, restaurant.longCoord))
+                      : ""
+                font.pixelSize: BVApp.Theme.fontSizeExtraSmall
+                color: BVApp.Theme.highlightColor
+                horizontalAlignment: Text.AlignRight
+
+                anchors {
+                    top: parent.top
+                    right: parent.right
+                    left: streetLabel.right
+
+                    rightMargin: BVApp.Theme.horizontalPageMargin
+                }
+            }
         }
 
         BVApp.IconToolBar {
@@ -115,7 +123,7 @@ BVApp.Page {
             anchors {
                 left: parent.left
                 right: parent.right
-                top: distanceLabel.bottom
+                top: underHeaderBar.bottom
 
                 leftMargin: BVApp.Theme.horizontalPageMargin
                 rightMargin: BVApp.Theme.horizontalPageMargin
@@ -173,8 +181,6 @@ BVApp.Page {
                 margins: BVApp.Theme.paddingLarge
             }
         }
-        VerticalScrollDecorator {}
-
     }
 }
 

commit 1d74dc37a89313af0c5a6e47fe8dbe8e0d13117c

Fix broken truncation in VenueDescription
Some idiot broke this with 38538533a34b47aeb0f7ec017e564ae4883d50e0.

qml/pages/VenueDescription.qml
@@ -98,8 +98,10 @@ BVApp.Page {
                 anchors {
                     top: parent.top
                     left: parent.left
+                    right: distanceLabel.left
 
-                    leftMargin: BVApp.Theme.horizontalPageMargin
+                    leftMargin:  BVApp.Theme.horizontalPageMargin
+                    rightMargin: BVApp.Theme.paddingSmall
                 }
             }
 
@@ -116,7 +118,6 @@ BVApp.Page {
                 anchors {
                     top: parent.top
                     right: parent.right
-                    left: streetLabel.right
 
                     rightMargin: BVApp.Theme.horizontalPageMargin
                 }