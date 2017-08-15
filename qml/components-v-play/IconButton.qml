micuintus/harbour-Berlin-Vegan

commit ac1bf12b676341708fc368abb6ad0ced515d34e0

[v-play] Clean up IconButton.qml code

qml/components-v-play/IconButton.qml
@@ -10,9 +10,7 @@ IconButton {
     property string type
     property string color
     property real scale
-    property real size
 
-    size: scale ? BVApp.Theme.iconSizeLarge * scale : BVApp.Theme.iconSizeLarge
 
     AppText {
         id: icn
@@ -23,14 +21,10 @@ IconButton {
         color: iconButton.color ? iconButton.color : BVApp.Theme.highlightColor
         font.family: BVApp.Theme.iconFor(type).fontFamily
 
-        font.pixelSize: size
+        font.pixelSize: scale ? BVApp.Theme.iconSizeLarge * scale : BVApp.Theme.iconSizeLarge
         horizontalAlignment: Text.AlignHCenter
-    }
-
-    Component.onCompleted: {
-        // HACK: if we are not the IconToolBar, we are the VenueMapPage
-        if (!Layout.alignment) {
-            icn.verticalAlignment = Text.AlignBottom
-        }
+        verticalAlignment:   iconButton.Layout.alignment ?
+                                 Text.AlignVCenter
+                               : Text.AlignBottom
     }
 }

commit 38af1c884831a18ca6a33165ba81babda49507cc

[v-play] Fix IconButton size, if scaled
Commit ac1bf12 broke the scaled version of the venue map marker in
v-play builds.

qml/components-v-play/IconButton.qml
@@ -21,7 +21,7 @@ IconButton {
         color: iconButton.color ? iconButton.color : BVApp.Theme.highlightColor
         font.family: BVApp.Theme.iconFor(type).fontFamily
 
-        font.pixelSize: scale ? BVApp.Theme.iconSizeLarge * scale : BVApp.Theme.iconSizeLarge
+        font.pixelSize: iconButton.scale ? BVApp.Theme.iconSizeLarge * iconButton.scale : BVApp.Theme.iconSizeLarge
         horizontalAlignment: Text.AlignHCenter
         verticalAlignment:   iconButton.Layout.alignment ?
                                  Text.AlignVCenter