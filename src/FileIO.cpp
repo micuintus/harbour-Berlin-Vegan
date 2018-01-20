f0d6ba59ad26147b949f4b0e205add024723d64c
Julian Vega <julian@veggi.es>
2018-01-20 15:57:38 +0100

    Add skeleton to handle write to/read from disk from QML
    Source:
    * https://askubuntu.com/a/454998
    
    // github issue #116

---
+++ src/FileIO.cpp
@@ -0,0 +1,1 @@
+#include "FileIO.h"
---
+++ src/FileIO.h
@@ -0,0 +1,16 @@
+#ifndef FILEIO_H
+#define FILEIO_H
+
+#pragma once
+
+#include <QObject>
+
+class FileIO : public QObject
+{
+    Q_OBJECT
+
+public:
+    FileIO() {}
+};
+
+#endif // FILEIO_H
