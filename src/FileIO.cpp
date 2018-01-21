06aba69e71a7048c2c2bfcf6c76b152396de384c
Julian Vega <julian@veggi.es>
2018-01-21 12:05:16 +0100

    Add read/write/exists methods to FileIO
    // github issue #116

--- src/FileIO.cpp
+++ src/FileIO.cpp
@@ -1,1 +1,55 @@
 #include "FileIO.h"
+
+bool FileIO::exists(const QString& filename)
+{
+    if (filename.isEmpty())
+        return false;
+
+    QString source = getWritableLocation() + filename;
+    QFile file(source);
+    if (file.exists() && file.open(QFile::ReadOnly))
+    {
+        file.close();
+        return true;
+    }
+
+    return false;
+}
+
+QString FileIO::read(const QString& filename)
+{
+    if (filename.isEmpty())
+        return "";
+
+    QString source = getWritableLocation() + filename;
+    QFile file(source);
+    if (!file.open(QFile::ReadOnly))
+        return "";
+
+    QTextStream in(&file);
+    QString data;
+    data = in.readAll();
+    file.close();
+    return data;
+}
+
+bool FileIO::write(const QString& filename, const QString& data)
+{
+    if (filename.isEmpty())
+        return false;
+
+    QString source = getWritableLocation() + filename;
+    QFile file(source);
+    if (!file.open(QFile::WriteOnly | QFile::Truncate))
+        return false;
+
+    QTextStream out(&file);
+    out << data;
+    file.close();
+    return true;
+}
+
+QString FileIO::getWritableLocation()
+{
+    return QStandardPaths::writableLocation(QStandardPaths::DataLocation) + QDir::separator();
+}
