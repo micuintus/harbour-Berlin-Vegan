c85e28d3197a7d1b0e3ec97aed14e887e94dc8dc
Julian Vega <julian@veggi.es>
2018-01-21 12:05:16 +0100

    Add read/write/exists methods to FileIO
    // github issue #116

--- src/FileIO.h
+++ src/FileIO.h
@@ -4,13 +4,25 @@
 #pragma once
 
 #include <QObject>
+#include <QDir>
+#include <QFile>
+#include <QTextStream>
+#include <QStandardPaths>
 
 class FileIO : public QObject
 {
     Q_OBJECT
 
+public slots:
+    bool exists(const QString& filename);
+    QString read(const QString& filename);
+    bool write(const QString& filename, const QString& data);
+
 public:
     FileIO() {}
+
+private:
+    QString getWritableLocation();
 };
 
 #endif // FILEIO_H
