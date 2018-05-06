29b553d0203e89e5b7c6a657fbf5b8e6a7b9464b
micu <micuintus@gmx.de>
2018-05-06 11:22:40 +0200

    Rename OpeningHoursModel.restaurant to openingData
    to better reflect the actual meaning of the property.
    
    // Gitub issue #222

--- src/OpeningHoursModel.h
+++ src/OpeningHoursModel.h
@@ -13,7 +13,7 @@ class OpeningHoursModel : public QStringListModel
 {
     Q_OBJECT
 
-    Q_PROPERTY(QJSValue restaurant READ restaurant WRITE setRestaurant)
+    Q_PROPERTY(QJSValue openingData READ  openingData WRITE setOpeningData)
     Q_PROPERTY(int count READ count)
     Q_PROPERTY(bool isOpen MEMBER m_isOpen NOTIFY isOpenChanged)
 
@@ -30,15 +30,15 @@ public:
     int rowCount(const QModelIndex &parent = QModelIndex()) const;
 
 signals:
-    void restaurantChanged();
+    void openingDataChanged();
     void isOpenChanged();
 
 private:
-    QJSValue restaurant() const { return m_restaurant; }
+    QJSValue openingData() const { return m_openingData; }
     int count() const { return m_data.size(); }
     void isOpen();
 
-    void setRestaurant(const QJSValue& restaurant);
+    void setOpeningData(const QJSValue& data);
 
     void mergeElements(const int from, const int to);
     void cleanUpOpeningHoursModel();
@@ -48,13 +48,12 @@ private:
     bool isInRange(const QString openingHours, const int currentMinute) const;
     int getMinute(const QString time) const;
 
-    QJSValue m_restaurant;
+    QJSValue m_openingData;
     bool m_isOpen;
 
     // we save 7 days with 2 each attributes (name and opening hours), e.g.
     //
     // QMap(("day", QVariant(QString, "Monday"))("hours", QVariant(QString, "12 - 22")))
     //
-    // Syntax from: http://www.cplusplus.com/forum/beginner/12409/
     QVector<QVector<QString>> m_data = QVector<QVector<QString>>(7, QVector<QString>(2, 0));
 };
