06779f5f434e0c491e964b7f4fa014cf91c3b936
micu <micuintus@gmx.de>
2018-05-23 22:33:31 +0200

    Copy opening hours string parse algorithms
    from OpeningHoursModel.cpp to OpeningHoursAlgorithms.h
    and apply them on loading the JSON database, and store
    the results under a new role 'OpeningMinutes'.
    
    // Github issue #234

--- src/OpeningHoursAlgorithms.h
+++ src/OpeningHoursAlgorithms.h
@@ -6,6 +6,7 @@
 #include <QVariant>
 #include <QtQml/QJSValue>
 
+// Condense opening hours part --->
 
 QVariantMap mergeElements(const QVariantList& openingHours, const unsigned from, const unsigned until)
 {
@@ -25,7 +26,6 @@ QVariantMap mergeElements(const QVariantList& openingHours, const unsigned from,
         return result;
     }
 }
-
 
 QVariantList condenseOpeningHours(const QVariantList& uncondensedOpeningHours)
 {
@@ -67,7 +67,6 @@ QString hoursString(const QJSValue& from, const QString& property)
     }
 }
 
-
 QVariantList extractOpenHoursData(const QJSValue& from)
 {
     QVariantList uncondensedOpeningHours
@@ -91,10 +90,93 @@ QVariantList extractOpenHoursData(const QJSValue& from)
     return uncondensedOpeningHours;
 }
 
+// <--- Condense opening hours part
+
+// Extract machine readable hours part --->
+
+int getMinute(const QString time)
+{
+    if (time == NULL || time.isEmpty())
+    {
+        return 0;
+    }
+
+    auto hour = 0;
+    auto minute = 0;
+    if (time.contains(":"))
+    {
+        const auto parts = time.split(":");
+        hour = parts[0].trimmed().toInt();
+        minute = parts[1].trimmed().toInt();
+    }
+    else
+    {
+        hour = time.trimmed().toInt();
+    }
+
+    return hour * 60 + minute;
+}
+
+QVariantMap parseOpeningMinutes(const QString& openingString)
+{
+    unsigned startMinute = 0;
+    unsigned endMinute = 0;
+    if (openingString.contains("-"))
+    {
+        const auto parts = openingString.split("-");
+        const auto startTime = parts[0];
+        startMinute = getMinute(startTime);
+        if (parts.size() > 1)
+        {
+            const auto endTime = parts[1];
+            endMinute = getMinute(endTime);
+            if (startMinute != 0 && endMinute == 0)
+            {
+                endMinute = 24 * 60;
+            }
+        }
+        else if (startMinute != 0)
+        {
+            endMinute = 24 * 60;
+        }
+    }
+
+    auto endMinutes = endMinute;
+    if (endMinute < startMinute) // closing time is after midnight
+    {
+        endMinutes = endMinutes + 24 * 60;
+    }
+
+    return QVariantMap
+    {
+        { "startMinute", startMinute},
+        { "endMinute",   endMinutes }
+    };
+}
+
+QVariantList extractOpeningMinutes(const QVariantList& openingHours)
+{
+    QVariantList openingMinutes;
+    std::transform(openingHours.begin(), openingHours.end(), std::back_inserter(openingMinutes),
+                   [](const QVariant& openingLine)
+    {
+        const QString& openingString = openingLine.toMap()["hours"].toString();
+        return parseOpeningMinutes(openingString);
+    });
+
+    return openingMinutes;
+}
+
+
+// <--- Extract machine readable hours part
+
 void extractAndProcessOpenHoursData(QStandardItem& to, const QJSValue& from)
 {
     auto const openingHours = extractOpenHoursData(from);
     auto const condensedOpeningHours = condenseOpeningHours(openingHours);
 
     to.setData(condensedOpeningHours, VenueModel::CondensedOpeningHours);
+
+    auto const openingMinutes = extractOpeningMinutes(openingHours);
+    to.setData(openingMinutes, VenueModel::OpeningMinutes);
 }
--- src/VenueModel.h
+++ src/VenueModel.h
@@ -120,6 +120,7 @@ public:
 
         // OpeningHours
         CondensedOpeningHours,
+        OpeningMinutes,
         OtMon,
         OtTue,
         OtWed,
