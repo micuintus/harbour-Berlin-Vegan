2ad490475c48f3d49e37421e0062fe358dcf7397
Heimen Stoffels <vistausss@outlook.com>
2018-06-17 19:01:17 +0000

    Added Dutch translation

---
+++ translations/harbour-berlin-vegan-nl.ts
@@ -0,0 +1,361 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL">
+<context>
+    <name></name>
+    <message id="id-monday">
+        <location filename="../src/OpeningHoursModel.cpp" line="197"/>
+        <source>Monday</source>
+        <translation>Maandag</translation>
+    </message>
+    <message id="id-tuesday">
+        <location filename="../src/OpeningHoursModel.cpp" line="200"/>
+        <source>Tuesday</source>
+        <translation>Dinsdag</translation>
+    </message>
+    <message id="id-wednesday">
+        <location filename="../src/OpeningHoursModel.cpp" line="203"/>
+        <source>Wednesday</source>
+        <translation>Woensdag</translation>
+    </message>
+    <message id="id-thursday">
+        <location filename="../src/OpeningHoursModel.cpp" line="206"/>
+        <source>Thursday</source>
+        <translation>Donderdag</translation>
+    </message>
+    <message id="id-friday">
+        <location filename="../src/OpeningHoursModel.cpp" line="209"/>
+        <source>Friday</source>
+        <translation>Vrijdag</translation>
+    </message>
+    <message id="id-saturday">
+        <location filename="../src/OpeningHoursModel.cpp" line="212"/>
+        <source>Saturday</source>
+        <translation>Zaterdag</translation>
+    </message>
+    <message id="id-sunday">
+        <location filename="../src/OpeningHoursModel.cpp" line="215"/>
+        <source>Sunday</source>
+        <translation>Zondag</translation>
+    </message>
+    <message id="id-closed">
+        <location filename="../src/OpeningHoursModel.cpp" line="261"/>
+        <source>closed</source>
+        <translation>gesloten</translation>
+    </message>
+    <message id="id-yes">
+        <location filename="../qml/components-generic/VenueDescriptionAlgorithms.js" line="35"/>
+        <source>yes</source>
+        <translation>ja</translation>
+    </message>
+    <message id="id-no">
+        <location filename="../qml/components-generic/VenueDescriptionAlgorithms.js" line="37"/>
+        <location filename="../qml/components-generic/VenueDescriptionAlgorithms.js" line="83"/>
+        <source>no</source>
+        <translation>nee</translation>
+    </message>
+    <message id="id-unknown">
+        <location filename="../qml/components-generic/VenueDescriptionAlgorithms.js" line="39"/>
+        <location filename="../qml/components-generic/VenueDescriptionAlgorithms.js" line="68"/>
+        <location filename="../qml/components-generic/VenueDescriptionAlgorithms.js" line="76"/>
+        <location filename="../qml/components-generic/VenueDescriptionAlgorithms.js" line="81"/>
+        <source>unknown</source>
+        <translation>onbekend</translation>
+    </message>
+    <message id="id-omnivore">
+        <location filename="../qml/components-generic/VenueDescriptionAlgorithms.js" line="49"/>
+        <location filename="../qml/pages/VenueFilterSettings.qml" line="176"/>
+        <source>omnivore</source>
+        <translation>omnivoor</translation>
+    </message>
+    <message id="id-omnivore-declared">
+        <location filename="../qml/components-generic/VenueDescriptionAlgorithms.js" line="53"/>
+        <source>omnivore \n(vegan declared)</source>
+        <translation>omnivoor
+(veganistisch verklaard)</translation>
+    </message>
+    <message id="id-vegetarian">
+        <location filename="../qml/components-generic/VenueDescriptionAlgorithms.js" line="57"/>
+        <location filename="../qml/pages/VenueFilterSettings.qml" line="167"/>
+        <source>vegetarian</source>
+        <translation>vegetarisch</translation>
+    </message>
+    <message id="id-vegetarian-declared">
+        <location filename="../qml/components-generic/VenueDescriptionAlgorithms.js" line="61"/>
+        <source>vegetarian \n(vegan declared)</source>
+        <translation>vegetarisch
+(veganistisch verklaard)</translation>
+    </message>
+    <message id="id-vegan">
+        <location filename="../qml/components-generic/VenueDescriptionAlgorithms.js" line="65"/>
+        <location filename="../qml/pages/VenueFilterSettings.qml" line="157"/>
+        <source>vegan</source>
+        <translation>veganistisch</translation>
+    </message>
+    <message id="id-opening-hours">
+        <location filename="../qml/components-generic/VenueDetails.qml" line="56"/>
+        <source>Opening hours</source>
+        <translation>Openingstijden</translation>
+    </message>
+    <message id="id-food-details">
+        <location filename="../qml/components-generic/VenueDetails.qml" line="80"/>
+        <source>Food details</source>
+        <translation>Voedselinformatie</translation>
+    </message>
+    <message id="id-vegan-venue-category">
+        <location filename="../qml/components-generic/VenueDetails.qml" line="87"/>
+        <source>Category</source>
+        <translation>Categorie</translation>
+    </message>
+    <message id="id-organic">
+        <location filename="../qml/components-generic/VenueDetails.qml" line="93"/>
+        <location filename="../qml/pages/VenueFilterSettings.qml" line="195"/>
+        <source>Organic products</source>
+        <translation>Biologische producten</translation>
+    </message>
+    <message id="id-gluten-free">
+        <location filename="../qml/components-generic/VenueDetails.qml" line="100"/>
+        <location filename="../qml/pages/VenueFilterSettings.qml" line="204"/>
+        <source>Gluten-free options</source>
+        <translation>Glutenvrije opties</translation>
+    </message>
+    <message id="id-accessibility">
+        <location filename="../qml/components-generic/VenueDetails.qml" line="113"/>
+        <source>Accessibility</source>
+        <translation>Toegankelijkheid</translation>
+    </message>
+    <message id="id-wheelchair">
+        <location filename="../qml/components-generic/VenueDetails.qml" line="119"/>
+        <location filename="../qml/pages/VenueFilterSettings.qml" line="213"/>
+        <source>Wheelchair-friendly</source>
+        <translation>Rolstoeltoegankelijk</translation>
+    </message>
+    <message id="id-wheelchair-wc">
+        <location filename="../qml/components-generic/VenueDetails.qml" line="125"/>
+        <location filename="../qml/pages/VenueFilterSettings.qml" line="222"/>
+        <source>Wheelchair-accessible WC</source>
+        <translation>Rolstoeltoegankelijk toilet</translation>
+    </message>
+    <message id="id-high-chair">
+        <location filename="../qml/components-generic/VenueDetails.qml" line="131"/>
+        <location filename="../qml/pages/VenueFilterSettings.qml" line="231"/>
+        <source>High chair</source>
+        <translation>Kinderstoel</translation>
+    </message>
+    <message id="id-dogs-allowed">
+        <location filename="../qml/components-generic/VenueDetails.qml" line="137"/>
+        <location filename="../qml/pages/VenueFilterSettings.qml" line="240"/>
+        <source>Dogs allowed</source>
+        <translation>Honden toegestaan</translation>
+    </message>
+    <message id="id-venue-features">
+        <location filename="../qml/components-generic/VenueDetails.qml" line="143"/>
+        <location filename="../qml/pages/VenueFilterSettings.qml" line="190"/>
+        <source>Venue features</source>
+        <translation>Mogelijkheden</translation>
+    </message>
+    <message id="id-wifi">
+        <location filename="../qml/components-generic/VenueDetails.qml" line="149"/>
+        <location filename="../qml/pages/VenueFilterSettings.qml" line="249"/>
+        <source>WiFi</source>
+        <translation>Wi-Fi</translation>
+    </message>
+    <message id="id-outdoor-seats">
+        <location filename="../qml/components-generic/VenueDetails.qml" line="155"/>
+        <source>Seats outdoor</source>
+        <translation>Buiten zitten</translation>
+    </message>
+    <message id="id-indoor-seats">
+        <location filename="../qml/components-generic/VenueDetails.qml" line="161"/>
+        <source>Seats indoor</source>
+        <translation>Binnen zitten</translation>
+    </message>
+    <message id="id-catering">
+        <location filename="../qml/components-generic/VenueDetails.qml" line="167"/>
+        <location filename="../qml/pages/VenueFilterSettings.qml" line="258"/>
+        <source>Catering</source>
+        <translation>Catering</translation>
+    </message>
+    <message id="id-delivery">
+        <location filename="../qml/components-generic/VenueDetails.qml" line="173"/>
+        <location filename="../qml/pages/VenueFilterSettings.qml" line="267"/>
+        <source>Delivery service</source>
+        <translation>Bezorgdienst</translation>
+    </message>
+    <message id="id-berlin-vegan">
+        <location filename="../qml/components-v-play/Page.qml" line="10"/>
+        <location filename="../qml/cover/CoverPage.qml" line="72"/>
+        <location filename="../qml/pages/about/AboutBerlinVegan.qml" line="93"/>
+        <location filename="../qml/pages/VenueList.qml" line="40"/>
+        <source>Berlin-Vegan</source>
+        <translation>Berlijn-Vegan</translation>
+    </message>
+    <message id="id-license">
+        <location filename="../qml/pages/about/LicenseViewer.qml" line="69"/>
+        <source>License:</source>
+        <translation>Licentie:</translation>
+    </message>
+    <message id="id-view-license">
+        <location filename="../qml/pages/about/ThirdPartySoftware.qml" line="83"/>
+        <source>View license</source>
+        <translation>Licentie tonen</translation>
+    </message>
+    <message id="id-venue-list">
+        <location filename="../qml/harbour-berlin-vegan.qml" line="130"/>
+        <source>Venues</source>
+        <translation>Gelegenheid</translation>
+    </message>
+    <message id="id-favorites-venue-list">
+        <location filename="../qml/harbour-berlin-vegan.qml" line="149"/>
+        <source>Favorites</source>
+        <translation>Favorieten</translation>
+    </message>
+    <message id="id-filter">
+        <location filename="../qml/harbour-berlin-vegan.qml" line="167"/>
+        <source>Filter</source>
+        <translation>Filter</translation>
+    </message>
+    <message id="id-about-venue-list">
+        <location filename="../qml/harbour-berlin-vegan.qml" line="176"/>
+        <source>About</source>
+        <translation>Over</translation>
+    </message>
+    <message id="id-filter-page-title">
+        <location filename="../qml/pages/VenueFilterSettings.qml" line="49"/>
+        <source>Filter settings</source>
+        <oldsource>Filter</oldsource>
+        <translation>Filterinstellingen</translation>
+    </message>
+    <message id="id-venue-category">
+        <location filename="../qml/pages/VenueFilterSettings.qml" line="55"/>
+        <source>Venue type</source>
+        <translation>Soort gelegenheid</translation>
+    </message>
+    <message id="id-food">
+        <location filename="../qml/pages/VenueFilterSettings.qml" line="62"/>
+        <source>Food</source>
+        <translation>Eten</translation>
+    </message>
+    <message id="id-shopping">
+        <location filename="../qml/pages/VenueFilterSettings.qml" line="81"/>
+        <source>Shopping</source>
+        <translation>Winkelen</translation>
+    </message>
+    <message id="id-filter-vegan-category">
+        <location filename="../qml/pages/VenueFilterSettings.qml" line="152"/>
+        <source>Veg*an category</source>
+        <translation>Veg*ancategorie</translation>
+    </message>
+    <message id="id-filter-venue-sub-type">
+        <location filename="../qml/pages/VenueFilterSettings.qml" line="104"/>
+        <source>Venue sub type</source>
+        <translation>Subsoort</translation>
+    </message>
+    <message id="id-venue-subtype-restaurant">
+        <location filename="../qml/pages/VenueFilterSettings.qml" line="110"/>
+        <source>Restaurant</source>
+        <translation>Restaurant</translation>
+    </message>
+    <message id="id-venue-subtype-fastfood">
+        <location filename="../qml/pages/VenueFilterSettings.qml" line="120"/>
+        <source>Snack bar</source>
+        <translation>Snackbar</translation>
+    </message>
+    <message id="id-venue-subtype-cafe">
+        <location filename="../qml/pages/VenueFilterSettings.qml" line="130"/>
+        <source>Café</source>
+        <translation>Café</translation>
+    </message>
+    <message id="id-venue-subtype-icecream">
+        <location filename="../qml/pages/VenueFilterSettings.qml" line="140"/>
+        <source>Ice cream parlor</source>
+        <translation>IJssalon</translation>
+    </message>
+    <message id="id-search">
+        <location filename="../qml/components-v-play/SearchField.qml" line="15"/>
+        <source>Search...</source>
+        <translation>Zoeken...</translation>
+    </message>
+    <message id="id-about-page-title">
+        <location filename="../qml/pages/about/AboutBerlinVegan.qml" line="34"/>
+        <source>About</source>
+        <translation>Over</translation>
+    </message>
+    <message id="id-about-cross-platform-app">
+        <location filename="../qml/pages/about/AboutBerlinVegan.qml" line="115"/>
+        <source>Cross-platform app</source>
+        <translation>Cross-platform app</translation>
+    </message>
+    <message id="id-about-for-sailfish-and-ios">
+        <location filename="../qml/pages/about/AboutBerlinVegan.qml" line="129"/>
+        <source>for SailfishOS and iOS</source>
+        <translation>voor SailfishOS en iOS</translation>
+    </message>
+    <message id="id-version">
+        <location filename="../qml/pages/about/AboutBerlinVegan.qml" line="151"/>
+        <source>Version</source>
+        <translation>Versie</translation>
+    </message>
+    <message id="id-development">
+        <location filename="../qml/pages/about/AboutBerlinVegan.qml" line="169"/>
+        <source>Development:</source>
+        <translation>Ontwikkeling:</translation>
+    </message>
+    <message id="id-ui-design">
+        <location filename="../qml/pages/about/AboutBerlinVegan.qml" line="207"/>
+        <source>UI design:</source>
+        <translation>Ontwerp:</translation>
+    </message>
+    <message id="id-bvapp-is-free-software">
+        <location filename="../qml/pages/about/AboutBerlinVegan.qml" line="237"/>
+        <source>Berlin-Vegan for SailfishOS and iOS is &lt;a href=&apos;ref&apos;&gt;Free Software&lt;/a&gt;: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the license, or (at your option) any later version.</source>
+        <translation>Berlijn-Vegan voor SailfishOS en iOS is &lt;a href=&apos;ref&apos;&gt;Vrije Software&lt;/a&gt;: je mag de software heruitgeven en/of aanpassen onder de voorwaarden van de GNU General Public License, zoals gepubliceerd door de Free Software Foundation, versie 2 van de licentie, of (optioneel) een latere versie.</translation>
+    </message>
+    <message id="id-view-gplv2">
+        <location filename="../qml/pages/about/AboutBerlinVegan.qml" line="260"/>
+        <source>View GPLv2</source>
+        <translation>GPLv2 tonen</translation>
+    </message>
+    <message id="id-you-can-view-source-code-here">
+        <location filename="../qml/pages/about/AboutBerlinVegan.qml" line="280"/>
+        <source>You can view the source code here:</source>
+        <translation>Hier kun je de broncode bekijken:</translation>
+    </message>
+    <message id="id-many-thanks-to-bvapp-and-license">
+        <location filename="../qml/pages/about/AboutBerlinVegan.qml" line="338"/>
+        <source>Many thanks goes to the &lt;a href=&apos;ref&apos;&gt;editorial team of berlin-vegan.de&lt;/a&gt; for creating and maintaining the Berlin-Vegan project with its marvelous restaurant database, which this app uses. This content is released under the terms of the Attribution-NonCommercial 4.0 International Creative Commons license (CC BY-NC 4.0).</source>
+        <translation>Heel veel lof voor het &lt;a href=&apos;ref&apos;&gt;team van berlin-vegan.de&lt;/a&gt; voor het creëren en bijhouden van het Berlin-Vegan-project en zijn fantastische restaurantdatabase, waar deze app gebruik van maakt. De inhoud wordt verspreid onder de voorwaarden van de Attribution-NonCommercial 4.0 International Creative Commons-licentie (CC BY-NC 4.0).</translation>
+    </message>
+    <message id="id-view-cc-by-nc">
+        <location filename="../qml/pages/about/AboutBerlinVegan.qml" line="361"/>
+        <source>View CC BY-NC</source>
+        <translation>CC BY-NC tonen</translation>
+    </message>
+    <message id="id-thanks-to-qt">
+        <location filename="../qml/pages/about/AboutBerlinVegan.qml" line="400"/>
+        <source>Berlin-Vegan for iOS and SailfishOS is being developed with the free and open source cross-platform framework &lt;a href=&apos;ref&apos;&gt;Qt&lt;/a&gt;. While the app uses jolla&apos;s native Qt-based SDK on SailfishOS, on iOS it is realized with the cross-platform V-Play Engine.</source>
+        <translation>Berlijn-Vegan voor iOS en SailfishOS wordt ontwikkelt met het open-bron cross-platform framework &lt;a href=&apos;ref&apos;&gt;Qt&lt;/a&gt;. De app gebruikt Jolla&apos;s native Qt-based SDK op SailfishOS en het cross-platform V-Play Engine op iOS.</translation>
+    </message>
+    <message id="id-thanks-to-mapbox">
+        <location filename="../qml/pages/about/AboutBerlinVegan.qml" line="426"/>
+        <source>We would like to thank Mapbox for donating &lt;a href=&apos;ref&apos;&gt;their astonishing Mapbox GL Plugin&lt;/a&gt; to the Qt project, which makes hardware accelerated vector maps possible for this app.</source>
+        <translation>We willen Mapbox bedanken voor het doneren van &lt;a href=&apos;ref&apos;&gt;hun fantastische Mapbox GL-plug-in&lt;/a&gt; aan het Qt-project, wat hardware-aansturing van kaarten mogelijk maakt in deze app.</translation>
+    </message>
+    <message id="id-thanks-to-other-3rd-party-sw">
+        <location filename="../qml/pages/about/AboutBerlinVegan.qml" line="468"/>
+        <source>Moreover, this application greatfully makes use of the following third party Free Software projects:</source>
+        <translation>Deze app maakt dankbaar gebruik van de volgende vrije software-projecten van derde partijen:</translation>
+    </message>
+    <message id="id-no-results">
+        <location filename="../qml/pages/VenueList.qml" line="69"/>
+        <source>No filter or search results</source>
+        <translation>Geen filter- of zoekresultaten</translation>
+    </message>
+    <message id="id-venue-closed">
+        <location filename="../qml/components-generic/VenueListItem.qml" line="104"/>
+        <source>closed now</source>
+        <oldsource>closed\nnow</oldsource>
+        <translation>momenteel gesloten</translation>
+    </message>
+</context>
+</TS>
