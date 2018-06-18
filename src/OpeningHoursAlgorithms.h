0ec45b71813d8b3d4e64a552161e4e84d0c2ca49
micu <micuintus@gmx.de>
2018-06-18 12:44:38 +0200

    Fix #236:
    If it is short after midnight, we treat this day still as if it is the
    day before. But then we must also ask if the day before is a holiday
    for using the right day index, not this day.

--- src/OpeningHoursAlgorithms.h
+++ src/OpeningHoursAlgorithms.h
@@ -260,25 +260,26 @@ bool isPublicHoliday(const QDate &date)
     return false;
 }
 
-std::pair<unsigned char, unsigned> extractDayIndexAndMinute(const QDateTime& dateTime)
+std::pair<unsigned char, unsigned> extractDayIndexAndMinute(QDateTime dateTime)
 {
     const unsigned currentHour = dateTime.time().hour();
     unsigned currentMinute = currentHour * MINUTES_PER_HOUR + dateTime.time().minute();
 
     char dayIndex = static_cast<unsigned char>(dateTime.date().dayOfWeek() - 1);
 
-    if (isShortAfterMidnight(dateTime))
+    if (isShortAfterMidnight(dateTime))    // If that is the case, we treat this time as if from the day before:
     {
-        currentMinute += MINUTES_PER_DAY; // add a complete day
-        dayIndex--; // its short after midnight, so we use the opening hour from the day before
+        currentMinute += MINUTES_PER_DAY;  // We count the minutes starting from the day before
+        dayIndex--;                        // and we use the opening hour from the day before.
+        dateTime = dateTime.addDays(-1);
 
         if (dayIndex == -1)
         {
-            dayIndex = SUNDAY_INDEX; // sunday
+            dayIndex = SUNDAY_INDEX;
         }
     }
 
-    if (isPublicHoliday(dateTime.date())) // it is a holiday so take the opening hours from sunday
+    if (isPublicHoliday(dateTime.date()))
     {
         dayIndex = SUNDAY_INDEX;
     }
